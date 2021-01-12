456a9934b246401ed4b57359eff02bfe172d05dc
Leonardo <leo.guzman.cruz@gmail.com>
2021-01-12 17:42:07 -0500

    switch

---
+++ condicionales.js
@@ -0,0 +1,24 @@
+//CONDICIONALES
+
+/* if(condicion){
+// 	tarea a realizar
+// } else if(){
+
+}else{}*/
+
+var numero = 10;
+
+// if(numero > 2){
+// 	console.log('Numero si es mayor que 2');
+// }
+// else if(numero == 2){
+// 	console.log('Numero no es mayor que 2');
+// }
+// else{
+// 	console.log('Numero no es mayor o igual que 2')
+// }
+
+if(numero > 2 && numero = 10 ){
+	console.log('Se cumple la condicion');
+}
+
---
+++ switch.js
@@ -0,0 +1,15 @@
+var colorFavorito = 'griss';
+
+switch(colorFavorito){
+	case 'azul': console.log('El color favorito es azul');
+	break;
+
+	case 'rojo': console.log('El color favorito es rojo');
+	break;
+
+	case 'amarillo': console.log('El color favorito es amarillo');
+	break;
+
+	default: console.log('Tu color es el ' + colorFavorito);
+	break;
+}
