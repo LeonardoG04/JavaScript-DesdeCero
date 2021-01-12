aed549e0f1ea2ee4e3076afdf826a8cfe62ffe33
Leonardo <leo.guzman.cruz@gmail.com>
2021-01-12 18:04:10 -0500

    aleatorios

---
+++ aleatorio.js
@@ -0,0 +1,20 @@
+
+var numeroAlearotio = Math.round(6.621);
+
+console.log(numeroAlearotio);
+
+//Obtener número aleatorios
+
+	//Math.random()*10;
+
+//Redondear al numero entero menor
+
+	//Math.floor(Math.random()*10);
+
+//Redondear al numero entero mayor
+
+	//Math.ceil(Math.random()*10);
+
+//Redondear al numero entero mas cercano
+
+	Math.round(Math.random()*10);
