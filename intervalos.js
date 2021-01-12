05ea135ffca0377d5a15174b636cb24aa6c62786
Leonardo <leo.guzman.cruz@gmail.com>
2021-01-12 17:48:51 -0500

    intervalos

---
+++ intervalos.js
@@ -0,0 +1,13 @@
+// INTERVALOS
+
+var segundero = 0;
+
+setInterval(function(){
+	++segundero;
+	console.log(segundero);
+}, 1000);
+
+// function contar(){
+// 	++segundero;
+// 	console.log(segundero);
+// }
